ab037fda39b8f3590f6aea1e9e8c8c0e5ed5ced0
Dante <dante@linux.home>
2020-12-02 21:45:05 +0100

    more file deletion

--- client/Defi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DefiConfig(AppConfig):
-    name = 'Defi'
--- client/Faucet/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaucetConfig(AppConfig):
-    name = 'Faucet'
