b55b6e59b2f493fe0d5a734dbac93861a57a9693
Dante <dante@linux.home>
2020-12-02 21:45:42 +0100

    more file deletion

--- client/old/ERC20Token/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Erc20TokenConfig(AppConfig):
-    name = 'ERC20Token'
--- client/old/SimpleTextStorage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpletextstorageConfig(AppConfig):
-    name = 'SimpleTextStorage'
